7d8de371697ed07fd4af29f1d0633fedc8f1d037
GeorgeLuque <jlgomez274@gmail.com>
2024-06-10 21:03:53 -0400

    verificacion javascripts

--- core/app/layouts/layout.php
+++ core/app/layouts/layout.php
@@ -509,33 +509,32 @@
         <?php }?>
 
         
- 
+        <!--nada-->
         <script src="assets/js/vendor/bootstrap/bootstrap.min.js"></script>
-
-        <script src="assets/js/vendor/jRespond/jRespond.min.js"></script> 
-
+        <!--muestra pantalla blanca-->
+        <script src="assets/js/vendor/jRespond/jRespond.min.js"></script>
         <script src="assets/js/vendor/sparkline/jquery.sparkline.min.js"></script>
-
+        <!--quita barralateral iqz-->
         <script src="assets/js/vendor/slimscroll/jquery.slimscroll.min.js"></script>
-
+        <!--muestra pantalla blanca-->
         <script src="assets/js/vendor/animsition/js/jquery.animsition.min.js"></script>
-
+        <!--nada-->
         <script src="assets/js/vendor/screenfull/screenfull.min.js"></script>
-
+        <!--nada-->
         <script src="assets/js/vendor/owl-carousel/owl.carousel.min.js"></script>
-
+        <!--nada-->
         <script src="assets/js/vendor/daterangepicker/moment.min.js"></script>
-
+        <!--nada-->
         <script src="assets/js/vendor/datetimepicker/js/bootstrap-datetimepicker.min.js"></script>
-
-       
+        <!--nada-->       
         <script src="assets/js/vendor/chosen/chosen.jquery.min.js"></script>
-       
-
+        <!--nada-->    
         <script src="assets/js/vendor/summernote/summernote.min.js"></script>
-
+        <!--nada-->  
         <script src="assets/js/vendor/coolclock/coolclock.js"></script>
+         <!--nada--> 
         <script src="assets/js/vendor/coolclock/excanvas.js"></script>
+        <!--nada--> 
         <script src="assets/js/vendor/footable/footable.all.min.js"></script>
         <!--/ vendor javascripts -->
 
